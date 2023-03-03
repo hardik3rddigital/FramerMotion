2f3e5b2298d032a525f9abd9c6769d46e49d65d7
hardik3rddigital <hardik.s@3rddigital.com>
2023-03-03 19:08:05 +0530

    type in damping and stiffness

--- src/File1.js
+++ src/File1.js
@@ -8,7 +8,7 @@ function File1() {
         className="box_one"
         animate={{ x: 800, opacity: 1 }}
         initial={{ opacity: 0.1 }}
-        transition={{ type: "tween", duration: 2 }}
+        transition={{ type: "spring", stiffness: 10, damping: 10 }}
       ></motion.div>
     </div>
   );
