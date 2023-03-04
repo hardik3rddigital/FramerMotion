030ba5118991e0d553b70e9f9029f36ae75d9ccf
hardik3rddigital <hardik.s@3rddigital.com>
2023-03-04 12:07:49 +0530

    Variants

--- src/File3.js
+++ src/File3.js
@@ -2,9 +2,44 @@ import React from "react";
 import { motion } from "framer-motion";
 
 function File3() {
+  // const boxVariant = {
+  //   objectOne: {
+  //     x: 100,
+  //     scale: 1.5,
+  //   },
+  //   objectTwo: {
+  //     x: 1000,
+  //     scale: 0.2,
+  //   },
+  // };
+
+  const list = {
+    hidden: { opacity: 0 },
+    visible: {
+      x: 100,
+      opacity: 1,
+      transition: { delay: 2, when: "beforeChildren" },
+    },
+  };
+
+  const list2 = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { x: 0, opacity: 1, staggerChildren: 0.2 },
+  };
+  // const item = { hidden: { x: -10, opacity: 0 } };
   return (
     <div className="boxcontainer">
-      <motion.div className="box_three box"></motion.div>
+      <motion.div
+        className="box"
+        variants={list}
+        animate="visible"
+        initial="hidden"
+        transition={{ type: "spring" }}
+      >
+        {[1, 2, 3].map((box) => {
+          return <motion.li className="boxItem" variants={list2}></motion.li>;
+        })}
+      </motion.div>
     </div>
   );
 }
