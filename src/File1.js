b6b4609821057a997f2041d3555c0d859499ace7
hardik3rddigital <hardik.s@3rddigital.com>
2023-03-04 10:26:13 +0530

    Onclick Animate Framer Motion

--- src/File1.js
+++ src/File1.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 function File1() {
+  const [isanimate, setIsanimate] = useState(false);
+
   return (
     <div className="boxcontainer">
       <motion.div
         className="box_one"
-        animate={{ x: 800, opacity: 1 }}
-        initial={{ opacity: 0.1 }}
-        transition={{ type: "spring", stiffness: 10, damping: 10 }}
+        animate={{
+          x: isanimate ? 750 : 0,
+          opacity: isanimate ? 1 : 0.4,
+          backgroundColor: isanimate ? "Blue" : "skyblue",
+          rotate: isanimate ? 360 : 0,
+        }}
+        // initial={{ opacity: 0.2, backgroundColor: "Red" }}
+        transition={{ type: "spring", stiffness: 60, damping: 20 }}
+        onClick={() => setIsanimate(!isanimate)}
       ></motion.div>
     </div>
   );
